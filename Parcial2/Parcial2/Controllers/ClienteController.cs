cc720f1be7cb2c1c727f144715dd3316ed4f833c
CAMILA DUSSAN CUELLAR <camila-dussanc@unilibre.edu.co>
2018-10-12 09:52:41 -0500

    Profe este es mi parcial el mejor de todos, carpeta Parcial2

---
+++ Parcial2/Parcial2/Controllers/ClienteController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Parcial2.Models;
+
+namespace Parcial2.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Random()
+        {
+            var Cliente = new Cliente()
+            {
+                Nombre = "Camila",
+                Apellido = "Dussan",
+                sueldo = 500
+            };
+            return View(Cliente);
+        }
+
+
+    }
+}
+    
---
+++ Parcial2/Parcial2/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parcial2.Models
+{
+    public class Cliente
+    {
+        public int ID { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public float sueldo { get; set; }
+
+    }
+}
